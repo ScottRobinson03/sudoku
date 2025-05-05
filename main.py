547ac60e26cf75fbb22bff993c2da43ecbe6a05f
Scott Robinson <scott.robinson2@sky.uk>
2025-05-05 17:04:13 -0500

    Add is_valid_sudoku() utility

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
+def is_valid_sudoku(board: list[list[int]], *, allow_empty: bool) -> bool:
+    def get_subgrid(board: list[list[int]], row_indx: int, col_indx: int):
+        start_row = (row_indx // 3) * 3
+        end_row = start_row + 3
+
+        start_col = (col_indx // 3) * 3
+        end_col = start_col + 3
+
+        subgrid = []
+        for i in range(start_row, end_row):
+            for j in range(start_col, end_col):
+                subgrid.append(board[i][j])
+        return subgrid
+
+    def get_column(board: list[list[int]], col_indx: int):
+        return [row[col_indx] for row in board]
+
+    for row_indx, row in enumerate(board):
+        if row_indx > 8:
+            # Too many rows
+            return False
+
+        for col_indx, number in enumerate(row):
+            if col_indx > 8:
+                # Too many columns
+                return False
+
+            if number < (0 if allow_empty else 1) or number > 9:
+                # Invalid number
+                return False
+
+            if row.count(number) > 1:
+                # Duplicate number in row
+                return False
+
+            if get_column(board, col_indx).count(number) > 1:
+                # Duplicate number in column
+                return False
+
+            if get_subgrid(board, row_indx, col_indx).count(number) > 1:
+                # Duplicate number in subgrid (3x3 grid)
+                return False
+
+    return True
+
+
 def draw_sudoku_to_terminal(board: list[list[int]]):
     num_rows = len(board)
     num_cols = len(board[0]) if num_rows > 0 else 0
@@ -66,7 +112,7 @@ def main():
         [9, 1, 2, 3, 4, 5, 6, 7, 8],
     ]
     draw_sudoku_to_terminal(board)
-
+    print(is_valid_sudoku(board, allow_empty=False))
 
 if __name__ == "__main__":
     main()
